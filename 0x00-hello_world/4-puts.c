ee156db069cb716f0d968ebe3f366743c70e72fa
VeadyEm <mveady@gmail.com>
2022-10-15 12:31:04 +0000

    my first line of c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle" using put function
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
+}
