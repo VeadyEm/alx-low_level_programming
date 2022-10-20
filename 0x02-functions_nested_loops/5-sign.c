d500b071c1a01685799be771645fc4dbc429d585
VeadyEm <mveady@gmail.com>
2022-10-20 00:14:20 +0000

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
   * print_sign - print the sign of a num
   * @n: the num to be checked for
   * Return: 1 and prints + if n is greater than zero
-  * 0 and prints 0 if n is zero
-  * -1 and prints - if n is less than zero
+  *	0 and prints 0 if n is zero
+  *	-1 and prints - if n is less than zero
   */
 
 int print_sign(int n)
@@ -15,11 +15,11 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	 else if (n ==0)
-	 {
-		 _putchar('0');
-		 return (0);
-	 }
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
 	{
 		_putchar('_');
