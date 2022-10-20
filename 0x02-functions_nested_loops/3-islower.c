e07d38b30313fd6affe8ce6ef0ceff04c2b3af7e
VeadyEm <mveady@gmail.com>
2022-10-20 00:06:59 +0000

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,10 +7,14 @@
   * Return: 1 if char is lowercase, otherwise 0.
   */
 
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
